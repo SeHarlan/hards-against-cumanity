530553f8eae658f1e7b0e40fc2f213ab814e769d
Scott <seharlan@gmail.com>
2020-07-11 10:15:41 -0700

    pathing test

--- pages/api/pathnames.js
+++ pages/api/pathnames.js
@@ -1,11 +1,12 @@
-const roomNames = ['init']
+const roomNames = ['community']
 
 export default function handler(req, res) {
   if (req.method === 'POST') {
     roomNames.push(req.body)
-    console.log('Room Names API: ', roomNames)
+    console.log('Room Names API(post): ', roomNames)
     res.send(JSON.stringify(roomNames))
   } else {
+    console.log('Room Names API(get): ', roomNames)
     res.send(JSON.stringify(roomNames))
   }
 }
