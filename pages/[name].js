2263111cf618acfa80233fd557b5aa8c7b74fc1d
Scott <seharlan@gmail.com>
2020-07-10 22:30:41 -0700

    config change

--- pages/[name].js
+++ pages/[name].js
@@ -3,7 +3,7 @@ import GameTable from './game-table'
 export default GameTable
 
 export async function getStaticPaths() {
-  const res = await fetch(`${process.env.URL_BASE}api/pathnames`)
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL_BASE}api/pathnames`)
   const pathnames = await res.json()
   const paths = pathnames.map(name => ({ params: { name } }))
 
