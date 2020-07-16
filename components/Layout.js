a22fd9dcba3bbcebed6fd20e0c2b91d92f58cb70
Scott <seharlan@gmail.com>
2020-07-16 09:38:31 -0700

    reverting to prev og:image

--- components/Layout.js
+++ components/Layout.js
@@ -14,9 +14,17 @@ export default function Layout({ children, home }) {
             name="description"
             content="Cards Against Humanity Online. Not associated with the actual company, plz buy their stuff!"
           />
-          <meta name="og:image" content='../public/images/preview.png' />
+
           <meta name="og:title" content={siteTitle} />
           <meta name="twitter:card" content="summary_large_image" />
+          {/* <meta name="og:image" content='../public/images/preview.png' /> */}
+          <meta
+            property="og:image"
+            content={`https://og-image.now.sh/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
 
           <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;500;700&display=swap" rel="stylesheet" />
